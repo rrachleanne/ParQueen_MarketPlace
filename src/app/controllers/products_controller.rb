881d2585185ef92d36aed08098dea19f9a388b97
rachel williams <rachel.williams@leapdev.io>
2020-07-27 18:31:47 +1000

    fixed profile page styling

--- src/app/controllers/products_controller.rb
+++ src/app/controllers/products_controller.rb
@@ -8,7 +8,17 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+
+    if user_signed_in?
+      if current_user.profile
+        redirect_to profile_path(current_user.profile.id)
+      else 
+        redirect_to new_profile_path
+    end
+    else
+      redirect_to new_user_session_path
+    end
+    
   end
 
   # GET /products/1
