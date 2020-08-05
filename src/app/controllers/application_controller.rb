743e397759ce39b7fe7b3fb0155bbda27ae8f8ca
rachel williams <rachel.williams@leapdev.io>
2020-08-05 18:45:59 +1000

    resolved final login bug

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
-  
-  
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || profiles_path
+  end
+
 
     def not_found
         raise ActionController::RoutingError.new('Not Found')
--- src/app/controllers/home_controller.rb
+++ src/app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
-  def after_sign_in_path_for(resource)
-    profile_path(current_user.profile.id)
-  end
-  
+
+
   
   def page
     
